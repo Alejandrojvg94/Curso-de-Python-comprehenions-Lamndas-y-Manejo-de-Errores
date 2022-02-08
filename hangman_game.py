7f9949a2b6500d1c0fee2643c0d718fb94c3b37e
Alejandro Velasco <alejandrovelasco094@gmail.com>
2022-02-08 01:40:41 -0500

    Version de juego incorporado, muestra y copia de solucion

--- hangman_game.py
+++ hangman_game.py
@@ -1,58 +1,52 @@
 import random
 import os
 
-os.system("clear")
-print("¡Adivina la palabra!")
-word = []
-answer = {}
-print(answer)
-di = {}
 
-
-def read_data():
-
-    with open("./Archivos/data.txt", "r", encoding="utf-8") as d:
+def read_data(file_data="./Archivos/data.txt"):
+    word = []
+    with open(file_data, "r", encoding="utf-8") as d:
         for i in d:
-            i = i.strip("\n")
-            word.append(i)
-        return random.choice(word)
-
-
-rand = read_data()
-print(rand)
-
-
-def ciclo():
-    answer_values = answer.values()
-    answer_values = list(answer_values)
-    answer_values = "".join(answer_values)
-    return answer_values
+            word.append(i.strip().upper())
+    return word
 
 
 def dics():
+    data = read_data(file_data="./Archivos/data.txt")
+    rand = random.choice(data)
+    rand_list = [letter for letter in rand]
+    rand_list_underscores = ["_"] * len(rand_list)
+    letter_index_dic = {}
 
-    for i in list(enumerate(rand)):
-        k = i[0]
-        v = i[1]
-        di[k] = v
-    for i in di:
+    for idx, letter in enumerate(rand):
+        if not letter_index_dic.get(letter):
+            letter_index_dic[letter] = []
+        letter_index_dic[letter].append(idx)
 
-        for j in range(0, i+1):
-            z = di.get(j)
+    while True:
+        print("¡Adivina la palabra! ")
 
-        lyrics = input("Ingresa una letra, por favor: ")
-        while lyrics != z:
-            lyrics = input("Ingresa una letra, por favor: ")
-            print(answer, "prueb", rand)
+        os.system("clear")
+        for element in rand_list_underscores:
+            print(element + " ", end="")
+        print("\n")
 
-            if lyrics == z:
-                answer[j] = z
+        letter = input("Ingresa una letra: ").strip().upper()
+        assert letter.isalpha(), "Solo puedes ingresar letras"
+
+        if letter in rand_list:
+            for idx in letter_index_dic[letter]:
+                rand_list_underscores[idx] = letter
+
+        if "_" not in rand_list_underscores:
+            # Si estÃ¡s en Unix (Mac o Linux) cambia cls por clear
+            os.system("clear")
+            print("¡Ganaste! La palabra era", rand)
+            break
 
 
 def run():
     read_data()
     dics()
-    ciclo()
 
 
 if __name__ == "__main__":
